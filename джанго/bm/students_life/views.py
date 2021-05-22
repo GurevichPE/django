267dc71a3d5b01cc2a76df7969be68dcc7300659
Guseva Yuliya <guseva.yuliya2002@yandex.ru>
2021-05-22 12:39:23 +0300

    + функция Юли

--- джанго/bm/students_life/views.py
+++ джанго/bm/students_life/views.py
@@ -37,52 +37,40 @@ class BookInLibList(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = BookInLib.objects.all()
         name = self.request.query_params.get('name', None)
+        bookname = self.request.query_params.get('book', None)
 
         try:
             name = name.upper()
         except BaseException:
-            name = ''
+            name = '//'
 
-        print(name, type(name))
-
+        try:
+            bookname = bookname.upper()
+        except BaseException:
+            bookname = '//'
 
         book_name = Book.objects.filter(
-            name__contains=name).first()  # все вхождение, которое встретил ORM при обращении к БД
-        exact = BookInLib.objects.all()
-        for boook in exact:
-            if boook.book.name == book_name.name:
-                print('book ' + str(book_name) + str(boook.id) + ' from ' + str(boook.lib) + ' was taken by students ' + str(boook.students) + ' times')
-        print('------------------------------------------------------')
+            name__contains=name).first()
 
+        exact = Book.objects.filter(
+            name__contains=bookname).first()
 
+       # exact = BookInLib.objects.all()
+        try:
+            for boook in queryset:
+                if boook.book.name == book_name.name:
+                    print('book ' + str(book_name) + str(boook.id) + ' from ' + str(boook.lib) +
+                          ' was taken by students ' + str(boook.students) + ' times')
+        except AttributeError:
+            pass
 
-       # for book in students_queryset:
-     #       the_student_in_room = BookInLib.objects.filter(id=book.id).first()
-     #       print(the_student_in_room)
-     #       print('for in for')
-          #  rooms_capacity_of_this_student = BookInLib.objects.filter(id=book.id).filter(room__capacity=capacity).all()
-       #     for room_capacity in rooms_capacity_of_this_student:
-         #       print(room_capacity)
+        if type(exact) != None:
+            for boook in queryset:
+                if boook.book.name == exact.name:
+                    print('book ' + str(book_name) + str(boook.id) + ' from ' + str(boook.lib) +
+                          ' was taken by ' + str(boook.departments) + ' departments')
 
-        print('-------------------++++++++++++++++++-----------------')
         return queryset
 
     serializer_class = BookInLibSerializer
 
-# class WormList(generics.ListCreateAPIView):
-#     def get_queryset(self):
-#         queryset = Worm.objects.all()
-#
-#         symbols = self.request.query_params.get('symbols', None)
-#         if symbols is not None:
-#             try:
-#                 # symbols = symbols.upper()
-#                 # symbols = symbols.replace('Ч', 'ч')
-#                 # symbols = symbols.replace('Е', 'е')
-#
-#                 queryset = queryset.filter(name__icontains=symbols).order_by('name')
-#             except ValueError:
-#                 pass
-#         return queryset
-#
-#     serializer_class = WormSerializer
